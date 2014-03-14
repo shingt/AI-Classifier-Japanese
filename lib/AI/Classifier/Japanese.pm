d93402d80b4206719dbd1e654f0fbbe62ee91e04
shin-en <van.s.ver@gmail.com>
2014-03-15 02:47:54 +0900

    First commit

---
+++ lib/AI/Classifier/Japanese.pm
@@ -0,0 +1,39 @@
+package AI::Classifier::Japanese;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+AI::Classifier::Japanese - It's new $module
+
+=head1 SYNOPSIS
+
+    use AI::Classifier::Japanese;
+
+=head1 DESCRIPTION
+
+AI::Classifier::Japanese is ...
+
+=head1 LICENSE
+
+Copyright (C) shin-en.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+shin-en E<lt>van.s.ver@gmail.comE<gt>
+
+=cut
+
---
+++ t/00_compile.t
@@ -0,0 +1,9 @@
+use strict;
+use Test::More;
+
+use_ok $_ for qw(
+    AI::Classifier::Japanese
+);
+
+done_testing;
+
